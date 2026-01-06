a00f5293f9c84617418ff3f3fe2ebb6b6a5866b5
KAMAL NATH U <kamalnath0507@gmail.com>
2026-01-06 13:02:33 +0530

    Convert C++ code to Python for various algorithms

--- day1.py
+++ day1.py
@@ -117,6 +117,122 @@ int main(){
     return 0;
 }
 
+//patterns
+*****
+*****
+*****
+*****
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int i=0;i<4;i++){
+        for(int j=0;j<=4;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
+ 
+    return 0;
+}
+
+
+*
+**
+***
+****
+*****
+    
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int i=0;i<5;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
+1
+12
+123
+1234
+12345
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    
+ 
+    return 0;
+}
+
+1
+22
+333
+4444
+55555
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=i;j++){
+            cout<<i;
+        }
+        cout<<endl;
+    }
+    
+ 
+    return 0;
+}
+*****
+****
+***
+**
+*
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int i=1;i<=5;i++){
+        for(int j=6-i;j!=0;j--){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    
+ 
+    return 0;
+}
+12345
+1234
+123
+12
+1
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int i=1;i<=5;i++){
+        for(int j=1;j<=6-i;j++){
+            cout<<j;
+        }
+        cout<<endl;
+    }
+    
+ 
+    return 0;
+}
+
+
+   
+
     
     
+
 
