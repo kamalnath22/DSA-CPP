5b8b7bee76a273693c976f4f7db161384b29b985
KAMAL NATH U <kamalnath0507@gmail.com>
2026-04-20 17:16:46 +0530

    Time: 0 ms (100%), Space: 71.8 MB (51.6%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxi;
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum=max(nums[i],sum+nums[i]);
+            maxi=max(maxi,sum);
+
+        }
+      return maxi;  
+        }
+    
+};
