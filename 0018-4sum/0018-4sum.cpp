f2830d9d36f48089de706bd31f434bbbcae1c1a3
KAMAL NATH U <kamalnath0507@gmail.com>
2026-06-12 21:51:11 +0530

    Time: 34 ms (12.1%), Space: 17.3 MB (82.57%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,40 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>>result;
+        int n=nums.size();
         sort(nums.begin(),nums.end());
-        vector<vector<int>>ans;
-        for(int i=0;i<nums.size();i++){
-            if(i>0&&nums[i]==nums[i-1]) continue;
-            for(int j=i+1;j<nums.size();j++){
-            
-            int k=j+1;
-            int l=nums.size()-1;
-            if(j!=i+1&&nums[j]==nums[j-1]) continue;
-            
-            while(k<l){
-                long long sum=nums[i]+nums[j];
-                sum+=nums[k];
+        for(int i=0;i<n-3;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+        for(int j=i+1;j<n-2;j++){
+            if(j>i+1&& nums[j]==nums[j-1]) continue;
+            int l=j+1;
+            int r=nums.size()-1;
+            while(l<r){
+                long long sum=nums[i];
+                sum+=nums[j];
                 sum+=nums[l];
-                if(sum<target){
-                    k++;
+                sum+=nums[r];
+                if(sum==target){
+                    result.push_back({nums[i],nums[j],nums[l],nums[r]});
+                    while(l<r&&nums[l]==nums[l+1])l++;
+                    while(l<r&&nums[r]==nums[r-1])r--;
+                    l++;
+                    r--;
                 }
-                else if(sum>target){
-                    l--;
-                }
-                else{
-                    vector<int>temp={nums[i],nums[j],nums[k],nums[l]};
-                    ans.push_back(temp);
-                    k++;
-                    l--;
-                    while(k<l&&nums[k]==nums[k-1])k++;
-                    while(k<l&&nums[l]==nums[l+1])l--;
-
-
-                }
-            }}
+                else if(sum>target) r--;
+                else l++;
+            }
         }
-        return ans;
-
+        }
+        return result;
         
     }
 };
