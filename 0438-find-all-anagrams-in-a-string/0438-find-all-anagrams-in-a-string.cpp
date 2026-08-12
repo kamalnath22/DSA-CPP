31751138904f141ec80bd93f96d53b38dec4ae5c
KAMAL NATH U <kamalnath0507@gmail.com>
2026-08-12 15:28:30 +0530

    Time: 7 ms (48.33%), Space: 11.7 MB (36.8%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,27 +1,40 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-
-        vector<int> result;
-        vector<int> freqP(26,0);
-        vector<int> window(26,0);
-
-        for(char c : p)
-            freqP[c-'a']++;
-
-        int k = p.size();
-
-        for(int i=0;i<s.size();i++)
-        {
-            window[s[i]-'a']++;
-
-            if(i >= k)
-                window[s[i-k]-'a']--;
-
-            if(window == freqP)
-                result.push_back(i-k+1);
+        unordered_map<char,int>mp;
+        for(char c:p){
+            mp[c]++;
         }
-
+        int count=mp.size();
+        int i=0;
+        int j=0;
+        vector<int>result;
+        int k=p.length();
+        while(j<s.length()){
+            if(mp.find(s[j])!=mp.end()){
+                mp[s[j]]--;
+                if(mp[s[j]]==0){
+                    count--;
+                }
+            }
+            if(j-i+1<k){
+                j++;
+            }
+            else if(j-i+1==k){
+                if(count==0){
+                    result.push_back(i);
+                }
+                if(mp.find(s[i])!=mp.end()){
+                    mp[s[i]]++;
+                    if(mp[s[i]]==1){
+                        count++;
+                    }
+                }
+                i++;
+                j++;
+            }
+        }
         return result;
+        
     }
 };
